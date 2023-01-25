18908a8b0e925378b2fa6a78107f4297053e38c7
shubham <gaikwad7777@gmail.com>
2023-01-25 19:38:27 +0530

    navbar added

---
+++ client/src/App.js
@@ -0,0 +1,58 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
+
+export default App;
+// const NAV_ITEMS = [
+//   {
+//     label: "Home",
+//     to: "/",
+//   },
+//   {
+//     label: "Promise",
+//     to: "/promise",
+//   },
+//   {
+//     label: "Treatments",
+//     children: [
+//       {
+//         label: "Pain Relief",
+//         to: "/pain-relief",
+//       },
+//       {
+//         label: "Teeth Replacement",
+//         to: "/teeth-replacement",
+//       },
+//       {
+//         label: "Cosmetic Dentistry",
+//         to: "/cosmetic-dentistry",
+//       },
+//       {
+//         label: "Preventive Dentistry",
+//         to: "/preventive-dentistry",
+//       },
+//       {
+//         label: "Kids Dentistry",
+//         to: "/kids-dentistry",
+//       },
+//     ],
+//   },
+//   {
+//     label: "Patient Safety",
+//     to: "/patient-safety",
+//   },
+//   {
+//     label: "Blog",
+//     to: "/blog",
+//   },
+//   {
+//     label: "Book Appointment",
+//     to: "/book-appointment",
+//   },
+// ];
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,294 @@
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Stack,
+  Collapse,
+  Icon,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useColorModeValue,
+  useBreakpointValue,
+  useDisclosure,
+  useColorMode,
+  Switch,
+} from "@chakra-ui/react";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+//   ChevronRightIcon,
+  MoonIcon,
+  SunIcon,
+} from "@chakra-ui/icons";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  const { isOpen, onToggle } = useDisclosure();
+  const { toggleColorMode } = useColorMode();
+
+  return (
+    <Box>
+      <Flex
+        bg={useColorModeValue("white", "gray.800")}
+        color={useColorModeValue("gray.600", "white")}
+        boxShadow="lg"
+        rounded="md"
+        minH={"60px"}
+        py={{ base: 5 }}
+        px={{ base: 4 }}
+        borderBottom={1}
+        borderStyle={"solid"}
+        borderColor={useColorModeValue("gray.200", "gray.900")}
+        align={"center"}
+      >
+        <Flex
+          flex={{ base: 1, md: "auto" }}
+          ml={{ base: -2 }}
+          display={{ base: "flex", md: "none" }}
+        >
+          <IconButton
+            onClick={onToggle}
+            icon={
+              isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
+            }
+            variant={"ghost"}
+            aria-label={"Toggle Navigation"}
+          />
+        </Flex>
+        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
+          <Text
+            textAlign={useBreakpointValue({ base: "center", md: "left" })}
+            fontFamily={"heading"}
+            color={useColorModeValue("gray.800", "white")}
+          >
+            <NavLink to={"/"}>LOGO</NavLink>
+          </Text>
+
+          <Flex display={{ base: "none", md: "flex" }} ml={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+        <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={"flex-end"}
+          direction={"row"}
+          spacing={6}
+        >
+          <SunIcon />
+          <Switch colorScheme={"telegram"} onChange={toggleColorMode} />
+          <MoonIcon />
+        </Stack>
+      </Flex>
+
+      <Collapse in={isOpen} animateOpacity>
+        <MobileNav />
+      </Collapse>
+    </Box>
+  );
+}
+
+const DesktopNav = () => {
+  const linkColor = useColorModeValue("gray.600", "gray.200");
+  const linkHoverColor = useColorModeValue("pink.400", "white");
+  const popoverContentBgColor = useColorModeValue("white", "gray.800");
+
+  return (
+    <Stack direction={"row"} spacing={4}>
+      {NAV_ITEMS.map((navItem) => (
+        <Box key={navItem.label}>
+          <Popover trigger={"hover"} placement={"bottom-start"}>
+            <PopoverTrigger>
+              <NavLink to={navItem.to}>
+                <Text
+                  p={2}
+                  to={navItem.to}
+                  fontSize={"lg"}
+                  fontWeight={500}
+                  color={linkColor}
+                  _hover={{
+                    textDecoration: "none",
+                    color: linkHoverColor,
+                  }}
+                >
+                  {navItem.label}
+                </Text>
+              </NavLink>
+            </PopoverTrigger>
+
+            {navItem.children && (
+              <PopoverContent
+                border={0}
+                boxShadow={"xl"}
+                bg={popoverContentBgColor}
+                p={4}
+                rounded={"xl"}
+                minW={"sm"}
+              >
+                <Stack>
+                  {navItem.children.map((child) => (
+                    <DesktopSubNav key={child.label} {...child} />
+                  ))}
+                </Stack>
+              </PopoverContent>
+            )}
+          </Popover>
+        </Box>
+      ))}
+    </Stack>
+  );
+};
+
+const DesktopSubNav = ({ label, to, subLabel }) => {
+  return (
+    <NavLink
+      to={to}
+      role={"group"}
+      display={"block"}
+      p={2}
+      rounded={"md"}
+      _hover={{ bg: useColorModeValue("pink.50", "gray.900") }}
+    >
+      <Stack direction={"row"} align={"center"}>
+        <Box p={2}>
+          <Text
+            transition={"all .3s ease"}
+            _groupHover={{ color: "pink.400" }}
+            fontWeight={500}
+          >
+            {label}
+          </Text>
+          <Text fontSize={"sm"}>{subLabel}</Text>
+        </Box>
+        {/* <Flex
+          transition={"all .3s ease"}
+          transform={"translateX(-10px)"}
+          opacity={0}
+          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
+          justify={"flex-end"}
+          align={"center"}
+          flex={1}
+        >
+          <Icon color={"pink.400"} w={5} h={5} as={ChevronRightIcon} />
+        </Flex> */}
+      </Stack>
+    </NavLink>
+  );
+};
+
+const MobileNav = () => {
+  return (
+    <Stack
+      bg={useColorModeValue("white", "gray.800")}
+      p={4}
+      display={{ md: "none" }}
+    >
+      {NAV_ITEMS.map((navItem) => (
+        <MobileNavItem key={navItem.label} {...navItem} />
+      ))}
+    </Stack>
+  );
+};
+
+const MobileNavItem = ({ label, children, to }) => {
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <Stack spacing={4} onClick={children && onToggle}>
+      <NavLink to={to}>
+        <Flex
+          py={2}
+          justify={"space-between"}
+          align={"center"}
+          _hover={{
+            textDecoration: "none",
+          }}
+        >
+          <Text
+            fontWeight={600}
+            color={useColorModeValue("gray.600", "gray.200")}
+          >
+            {label}
+          </Text>
+          {children && (
+            <Icon
+              as={ChevronDownIcon}
+              transition={"all .25s ease-in-out"}
+              transform={isOpen ? "rotate(180deg)" : ""}
+              w={6}
+              h={6}
+            />
+          )}
+        </Flex>
+      </NavLink>
+
+      <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
+        <Stack
+          mt={2}
+          pl={4}
+          borderLeft={1}
+          borderStyle={"solid"}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+          align={"start"}
+        >
+          {children &&
+            children.map((child) => (
+              <NavLink key={child.label} py={2} to={child.to}>
+                {child.label}
+              </NavLink>
+            ))}
+        </Stack>
+      </Collapse>
+    </Stack>
+  );
+};
+
+const NAV_ITEMS = [
+  {
+    label: "Home",
+    to: "/",
+  },
+  {
+    label: "Promise",
+    to: "/promise",
+  },
+  {
+    label: "Treatments",
+    children: [
+      {
+        label: "Pain Relief",
+        to: "/pain-relief",
+      },
+      {
+        label: "Teeth Replacement",
+        to: "/teeth-replacement",
+      },
+      {
+        label: "Cosmetic Dentistry",
+        to: "/cosmetic-dentistry",
+      },
+      {
+        label: "Preventive Dentistry",
+        to: "/preventive-dentistry",
+      },
+      {
+        label: "Kids Dentistry",
+        to: "/kids-dentistry",
+      },
+    ],
+  },
+  {
+    label: "Patient Safety",
+    to: "/patient-safety",
+  },
+  {
+    label: "Blog",
+    to: "/blog",
+  },
+  {
+    label: "Book Appointment",
+    to: "/book-appointment",
+  },
+];
